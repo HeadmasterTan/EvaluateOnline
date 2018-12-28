149aded4866ab1b258078cb5c8629ae1a0992f30
tanzj <843811863@qq.com>
2018-12-28 11:58:48 +0800

    Front Code Commit

---
+++ front/src/views/index.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="main">
+        <Layout class="layout">
+            <Header v-show="!isLogin" class="header">
+                <div class="brand"><span>员工互评系统</span></div>
+                <div class="user">
+                    <span>{{userInfo.name}}</span> 丨
+                    <a class="logout">退出</a>
+                </div>
+            </Header>
+            <Content>
+                <router-view></router-view>
+            </Content>
+        </Layout>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                userInfo: {
+                    name: '大黄'
+                }
+            }
+        },
+        computed: {
+            isLogin() {
+                return this.$route.path.indexOf('login') > -1;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.main {
+    height: 100vh;
+    background-color: #f5f5f5;
+}
+.main .layout {
+    height: 100%;
+}
+.main .header {
+    overflow: hidden;
+    color: white;
+    font-size: 14px;
+}
+.main .brand {
+    float: left;
+    font-size: 22px;
+    position: relative;
+    height: 70%;
+    width: 250px;
+    background: rgba(255,255,255,.3);
+    border-radius: 4px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.main .user {
+    float: right;
+}
+.user .logout {
+    color: white;
+}
+.user .logout:hover {
+    color: #c5c5c5;
+}
+</style>
